9a07846d071e0bddc8bfa75b675f8c6910a07e5f
sabhareesh1009 <sabhareeshkandikonda1109@gmail.com>
2021-10-02 01:09:45 +0530

    Integrated the spotify app

--- musico/src/spotify.js
+++ musico/src/spotify.js
@@ -1,30 +1,31 @@
 // https://developer.spotify.com/documentation/web-playback-sdk/quick-start/
 
-export const authEndPoint = 
-    "https://accounts.spotify.com/authorize";
+export const authEndPoint = "https://accounts.spotify.com/authorize";
 
 const redirectUri = "http://localhost:3000/";
-const clientId = "d71b6f6ddde643a29ab42f5f5666bfcd";
+export const clientId = "d71b6f6ddde643a29ab42f5f5666bfcd";
+export const clientSecretId = "d71b6f6ddde643a29ab42f5f5666bfcd";
 
 const scopes = [
-    "user-read-currently-playing",
-    "user-read-recently-played",
-    "user-read-playback-state",
-    "user-top-read",
-    "user-modify-playback-state",
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
 ];
 
-export const getTokenFromUrl = ()=> {
-    return window.location.hash
+export const getTokenFromUrl = () => {
+  return window.location.hash
     .substring(1)
-    .split('&')
+    .split("&")
     .reduce((intial, item) => {
-        let parts = item.split('=');
-        intial[parts[0]] = decodeURIComponent(parts[1]);
-        return intial
-    }, {})
-}
+      let parts = item.split("=");
+      intial[parts[0]] = decodeURIComponent(parts[1]);
+      return intial;
+    }, {});
+};
 
 export const loginUrl = `${authEndPoint}?client_id=${clientId}&redirect_uri=
-${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
-
+${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
